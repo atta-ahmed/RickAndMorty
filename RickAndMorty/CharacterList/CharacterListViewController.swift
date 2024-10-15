762d2ebebc61a5fe5b7417e85c193a1926e8b8f1
Atta <atta7739@gmail.com>
2024-10-16 00:49:29 +0300

    make a first call from view controller

--- RickAndMorty/CharacterList/CharacterListViewController.swift
+++ RickAndMorty/CharacterList/CharacterListViewController.swift
@@ -6,26 +6,70 @@
 //
 
 import UIKit
+import SwiftUI
 
 class CharacterListViewController: UIViewController {
 
-    @IBOutlet weak var charactersTable: UITableView!
+    @IBOutlet weak var tableView: UITableView!
+    var viewModel: CharacterListViewModelProtocol = CharacterListViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+        fetchCharacters()
+        setupViewModel()
+    }
+    
+    private func setupTableView() {
         print("hello from charactersTable")
-        charactersTable.delegate = self
-        charactersTable.dataSource = self
+        tableView.register(CharacterTableViewCell.self, forCellReuseIdentifier: CharacterTableViewCell.reuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchCharacters() {
+     viewModel.fetchCharacters()
+    }
+      
+    
+    private func setupViewModel() {
+        viewModel.onReciveCharacter = { [weak self] in
+            print("-------onReciveCharacter -----", self?.viewModel.character(at: 0))
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        
+        viewModel.onError = { [weak self] (error: NetworkError) in
+            print("Error fetching orders:", error)
+            DispatchQueue.main.async {
+                // self?.showError(message: "Failed to load orders.")
+            }
+        }
     }
 
 }
 
 extension CharacterListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return viewModel.countOfCharcters ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CharacterTableViewCell.reuseIdentifier, for: indexPath) as? CharacterTableViewCell else {
+            return UITableViewCell() // Return empty if dequeue fails
+        }
+
+        // Get the character from the viewModel and configure the cell
+        if let character = viewModel.character(at: indexPath.row) {
+            cell.configure(with: character)
+        }
+
+        return cell
+     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150.0 // Adjust this value to the desired height
     }
+    
 }
